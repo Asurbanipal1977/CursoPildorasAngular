e760fb889b15e515d31e3fab49d7ffa0b026c67a
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-11-11 17:37:18 +0100

    Cambio en master de listado de empleados

--- src/app/services/empleados.service.ts
+++ src/app/services/empleados.service.ts
@@ -13,7 +13,7 @@ export class EmpleadosService {
     {nombre:"Pepe",apellido:"Sánchez",cargo:"Analista",salario:1500},
     {nombre:"David",apellido:"Pérez",cargo:"Programador",salario:1200},
     {nombre:"Miguel Ángel",apellido:"Martínez Eusebio",cargo:"Programador",salario:1400},
-    {nombre:"Julián",apellido:"Pérez",cargo:"Programador",salario:1600}
+    {nombre:"Julián",apellido:"Pérez Gutiérrez",cargo:"Programador",salario:1600}
   ];  
 
   listarEmpleados(): Empleado[]{
